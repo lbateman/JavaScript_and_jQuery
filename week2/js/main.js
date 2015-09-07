b7dd5cb01972699aed1f737631d91ef698998271
lbateman <lbateman@mcad.edu>
2015-09-06 20:25:18 -0400

    Week 2 Commit: Flashcard project
    Created new repo for class and added weekly folders as requested. New
    repo includes old week1 files and new week2 files for the state capitals
    flashcard project.

---
+++ week2/js/main.js
@@ -0,0 +1,64 @@
+// ******************** Alabama ********************
+
+// set up a variable to hold the state ID
+var alabama = document.getElementById('alabama') ;
+
+// set up the function to show an alert for the state capital
+var showCapitalAL = function(){
+    alert('Montgomery') ;
+} 
+
+// Set up a listener for the onmouseover event; when it happens, run the function
+alabama.addEventListener('mouseover', showCapitalAL) ;
+
+// ******************** Alaska ********************
+
+var alaska = document.getElementById('alaska') ;
+
+var showCapitalAK = function(){
+    alert('Juneau') ;
+} 
+
+alaska.addEventListener('mouseover', showCapitalAK) ;
+
+// ******************** Arizona ********************
+
+var arizona = document.getElementById('arizona') ;
+
+var showCapitalAZ = function(){
+    alert('Phoenix') ;
+} 
+
+arizona.addEventListener('mouseover', showCapitalAZ) ;
+
+// ******************** Arkansas ********************
+
+var arkansas = document.getElementById('arkansas') ;
+
+var showCapitalAR = function(){
+    alert('Little Rock') ;
+} 
+
+arkansas.addEventListener('mouseover', showCapitalAR) ;
+
+// ******************** California ********************
+
+var california = document.getElementById('california') ;
+
+var showCapitalCA = function(){
+    alert('Sacramento') ;
+} 
+
+california.addEventListener('mouseover', showCapitalCA) ;
+
+// ******************** Colorado ********************
+
+var colorado = document.getElementById('colorado') ;
+
+var showCapitalCO = function(){
+    alert('Denver') ;
+} 
+
+colorado.addEventListener('mouseover', showCapitalCO) ;
+
+// Please can we learn arrays now? :)
